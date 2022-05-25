5ece7eb911c34c4443ebd2359a6a4a23d2add105
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-05-25 15:32:15 +0530

    Update __main__.py

--- HarryPotter/__main__.py
+++ HarryPotter/__main__.py
@@ -27,7 +27,7 @@ HELPABLE = {}
 async def start_bot():
     global HELPABLE
     for module in ALL_MODULES:
-        imported_module = importlib.import_module("Rose.plugins." + module)
+        imported_module = importlib.import_module("HarryPotter.plugins." + module)
         if (
             hasattr(imported_module, "__MODULE__")
             and imported_module.__MODULE__
